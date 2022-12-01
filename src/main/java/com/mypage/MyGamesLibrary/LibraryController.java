6667d28840b0b87b9637401e674f42199b665afb
damiankadziela81 <damiankadziela02@gmail.com>
2022-12-01 12:27:12 +0100

    added 2nd endpoint and its method

--- src/main/java/com/mypage/MyGamesLibrary/LibraryController.java
+++ src/main/java/com/mypage/MyGamesLibrary/LibraryController.java
@@ -2,6 +2,7 @@ package com.mypage.MyGamesLibrary;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,5 +16,10 @@ class LibraryController {
     @GetMapping("/games")
     public List<Game> getAll(){
         return gameRepository.getAll();
+    }
+
+    @GetMapping("/games/{id}")
+    public Game getById(@PathVariable("id") int id){
+        return gameRepository.getById(id);
     }
 }

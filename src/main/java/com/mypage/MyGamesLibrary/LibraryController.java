b2f2825be87e50bf3601953adc7c4a9186e0883d
damiankadziela81 <damiankadziela02@gmail.com>
2022-12-01 12:13:46 +0100

    added first endpoint to list all DB entries

--- src/main/java/com/mypage/MyGamesLibrary/LibraryController.java
+++ src/main/java/com/mypage/MyGamesLibrary/LibraryController.java
@@ -1,13 +1,19 @@
 package com.mypage.MyGamesLibrary;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 class LibraryController {
 
-    @GetMapping("/test")
-    public int test(){
-        return 1;
+    @Autowired
+    GameRepository gameRepository;
+
+    @GetMapping("/games")
+    public List<Game> getAll(){
+        return gameRepository.getAll();
     }
 }
